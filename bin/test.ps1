1f3653ef94d50b8d07d36a39e96ad8b0b26f4974
Joe Huss <detain@interserver.net>
2022-07-10 06:04:10 -0400

    test update;

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,5 +1,7 @@
 #Requires -Modules @{ ModuleName = 'Pester'; MaximumVersion = '4.99' }
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-$result = Invoke-Pester "$PSScriptRoot/.." -PassThru
+
+$result = Invoke-Pester "$PSScriptRoot\.." -PassThru
+
 exit $result.FailedCount
