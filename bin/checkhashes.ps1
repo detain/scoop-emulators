776c3e03cd9834a7bd2b16df2f2480b72051dfd6
Joe Huss <detain@interserver.net>
2022-07-12 08:54:11 -0400

    bunch of updates

--- bin/checkhashes.ps1
+++ bin/checkhashes.ps1
@@ -1,4 +1,4 @@
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
 $checkhashes = "$env:SCOOP_HOME/bin/checkhashes.ps1"
 $dir = "$PSScriptRoot/../bucket" # checks the parent dir
-Invoke-Expression -command "& '$checkhashes' -dir '$dir' $($args | ForEach-Object { "$_ " })"
+Invoke-Expression -Command "& '$checkhashes' -Dir '$dir' $($args | ForEach-Object { "$_ " })"
